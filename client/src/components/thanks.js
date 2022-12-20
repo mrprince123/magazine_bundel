0761a2647b221e813a9b2128c6255a617b97ff4b
Prince <princekrdss2018@gmail.com>
2022-12-20 12:49:41 +0530

    Added styles in the thanks page

--- client/src/components/thanks.js
+++ client/src/components/thanks.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom';
 
 function Thanks() {
     return (
-        <div>
-            <img src="https://img.icons8.com/fluency/64/null/apple-mail.png"></img>
+        <div className='thanks_div'>
+            <img src="https://img.icons8.com/fluency/104/null/edit-message.png" alt='Think for pic'></img>
             <h1>Thanks for Submitting!</h1>
             <p>your message has been sent!</p>
-            <button>Go Home</button>
+            <button><NavLink className='nav-link' to="/">Go Home</NavLink></button>
         </div>
-    )
+    );
 }
 
 export default Thanks;
